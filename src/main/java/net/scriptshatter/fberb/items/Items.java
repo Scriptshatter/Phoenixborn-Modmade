8d02cfcaf7f7dddffa647cf5185344b6b3316e2e
Scriptshatter <busterboyelijah@gmail.com>
2023-02-08 13:51:46 -0500

    worked on item stuff

--- src/main/java/net/scriptshatter/fberb/items/Items.java
+++ src/main/java/net/scriptshatter/fberb/items/Items.java
@@ -4,8 +4,11 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.Identifier;
 import net.scriptshatter.fberb.Phoenix;
 import net.scriptshatter.fberb.blocks.Phoenix_blocks;
@@ -15,8 +18,14 @@ public class Items {
     public static final Phoenix_brooch PHOENIX_BROOCH = register_items("phoenix_brooch", new Phoenix_brooch(new FabricItemSettings()), Phoenix.MOD_ID);
     public static final Charged_amethyst CHARGED_AMETHYST = register_items("charged_amethyst", new Charged_amethyst(new FabricItemSettings()), Phoenix.MOD_ID);
     public static final Machine_item MACHINE_ITEM = register_items("machine", new Machine_item(), Phoenix.MOD_ID);
+    public static final Phoenix_axe PHOENIX_AXE = register_API_items("phoenix_axe", new Phoenix_axe(2, 2, ToolMaterials.DIAMOND, BlockTags.AXE_MINEABLE, 100, new FabricItemSettings()), Phoenix.MOD_ID);
 
     public static <I extends Item> I register_items(String name, I item, String modid){
+        return Registry.register(Registries.ITEM, new Identifier(modid, name), item);
+    }
+
+    public static <I extends Item> I register_API_items(String name, I item, String modid){
+        TempAPI.ITEM_WITH_TEMP.registerSelf(item);
         return Registry.register(Registries.ITEM, new Identifier(modid, name), item);
     }
 
---
+++ src/main/java/net/scriptshatter/fberb/mixin/ItemRendererMixin.java
@@ -0,0 +1,54 @@
+package net.scriptshatter.fberb.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.*;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolItem;
+import net.scriptshatter.fberb.items.Birb_item;
+import net.scriptshatter.fberb.items.TempAPI;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import software.bernie.geckolib.animatable.client.RenderProvider;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+    private void renderGuiQuad(BufferBuilder buffer, int x, int y, int width, int height, int red, int green, int blue, int alpha) {
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+        buffer.vertex((x), (y), 0.0).color(red, green, blue, alpha).next();
+        buffer.vertex((x), (y + height), 0.0).color(red, green, blue, alpha).next();
+        buffer.vertex((x + width), (y + height), 0.0).color(red, green, blue, alpha).next();
+        buffer.vertex((x + width), (y), 0.0).color(red, green, blue, alpha).next();
+        BufferRenderer.drawWithGlobalProgram(buffer.end());
+    }
+
+    @Inject(method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At("HEAD"))
+    public void itemModelHook(TextRenderer renderer, ItemStack stack, int x, int y, String countLabel, CallbackInfo ci){
+        if(!stack.isEmpty()){
+            Birb_item item = TempAPI.ITEM_WITH_TEMP.find(stack, null);
+            if (item != null) {
+                RenderSystem.disableDepthTest();
+                RenderSystem.disableTexture();
+                RenderSystem.disableBlend();
+                Tessellator tessellator = Tessellator.getInstance();
+                BufferBuilder bufferBuilder = tessellator.getBuffer();
+                int i = item.getItemBarStepTemp(stack);
+                int j = item.getItemBarColorTemp(stack);
+                this.renderGuiQuad(bufferBuilder, x + 2, y + 11, 13, 2, 0, 0, 0, 255);
+                this.renderGuiQuad(bufferBuilder, x + 2, y + 13, i, 1, j >> 16 & 255, (j >> 8 & 255)/5, j >> 8 & 255, 255);
+                RenderSystem.enableBlend();
+                RenderSystem.enableTexture();
+                RenderSystem.enableDepthTest();
+            }
+        }
+    }
+
+}
